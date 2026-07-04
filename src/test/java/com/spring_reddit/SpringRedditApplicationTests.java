83e1a9252dc3901626ed3c797d149ea44b1d6723
Vikram Singh <vikramsinghjogawat5@gmail.com>
2026-07-04 16:10:58 +0530

    Initial Commit

---
+++ src/test/java/com/spring_reddit/SpringRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
