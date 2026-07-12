c59d73436993dc0043a43d795183e02e3b32fe5b
Vikram Singh <vikramsinghjogawat5@gmail.com>
2026-07-13 00:36:58 +0530

    Added Comment & Repo

--- src/main/java/com/spring_reddit/model/Comment.java
+++ src/main/java/com/spring_reddit/model/Comment.java
@@ -1,9 +1,28 @@
 package com.spring_reddit.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String text;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId",referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId",referencedColumnName = "userId")
+    private User user;
 }
--- src/main/java/com/spring_reddit/repository/CommentRepository.java
+++ src/main/java/com/spring_reddit/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.spring_reddit.repository;
 
-public class CommentRepository {
+import com.spring_reddit.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
