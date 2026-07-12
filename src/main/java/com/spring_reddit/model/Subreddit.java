38c935968445cab8e73dc036ec979d8dccc9314c
Vikram Singh <vikramsinghjogawat5@gmail.com>
2026-07-12 18:14:01 +0530

    Added Subreddit fields with lombok annotations

--- src/main/java/com/spring_reddit/model/Subreddit.java
+++ src/main/java/com/spring_reddit/model/Subreddit.java
@@ -1,4 +1,31 @@
 package com.spring_reddit.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Subreddit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Community name is required.")
+    private String name;
+    @NotBlank(message = "Description is required.")
+    private String description;
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
